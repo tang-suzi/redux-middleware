b9dbd07015e9c949c7a64737d4dd062719d88bfd
tang-suzi <tangtang960225@163.com>
2018-09-03 22:04:24 +0800

    复习redux

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {createStore} from './redux/index'
+
+let counter = (state=0,action) => {
+    if(action){
+        switch(action.type){
+            case "ADD":
+                return state + 1;
+            case "SUB":
+                return state - 1;
+            default:
+                return state;
+        }
+    }else{
+        return state;
+    }
+}
+let store = createStore(counter);
+console.log(store.getState())
+store.dispatch({type:'ADD'})
+console.log(store.getState());
